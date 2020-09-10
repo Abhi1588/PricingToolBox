897107bb534f862879bf414324536ca208a77072
Abhi1588 <abhishek.oneplus.0105@gmail.com>
2020-09-10 15:53:11 +0530

    Update forward price function.

--- BSVanillaOptions.py
+++ BSVanillaOptions.py
@@ -2,6 +2,22 @@ import numpy as np
 import pandas as pd
 
 
-def forward_PriceBS(spot, strike, rate, maturity, volatility, dividend):
-    #Theorem 2.1
+def forward_PriceBS(spot, strike, rate, maturity, dividend, frequency = "annual"):
+
+    if frequency == "annual":
+        [x + 1 for x in range(0, 2)]
+        D = spot*
+
+    elif frequency == "semi":
+
+
+
+    # Theorem 2.1
     return np.exp(-rate*maturity)*(np.exp((rate-dividend)*maturity)*spot - strike)
+
+
+def main():
+    print(forward_PriceBS(100,120,0.05,2,0.01,0))
+
+
+main()
