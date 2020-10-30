8f95997cd85cdfd5541cd096fdbca5fa2824e5db
Abhi1588 <abhishek.oneplus.0105@gmail.com>
2020-10-30 20:11:17 +0530

    Practice python starting first problem

--- PythonPractice.py
+++ PythonPractice.py
@@ -9,14 +9,24 @@ a = np.random.randint(10,99,(20,20))
 
 def largest_product(a):
     prd = 0
-#    d = dict()
+    d = dict()
+    l = []
     for row in np.arange(0,a.shape[0]-3):
         for col in np.arange(0,a.shape[0]-3):
-            temp = a[row,col]*a[row+1,col+1]*a[row+2,col+2]*a[row+3,col+3]
+            temp_down = a[row, col]*a[row+1, col+1]*a[row+2, col+2]*a[row+3, col+3]
+            temp_up = a[row+3, col]*a[row+2, col+1]*a[row+1, col+2]*a[row, col+3]
+            if temp_up > temp_down:
+                temp = temp_up
+                address = f"Up Diag {(row+3, col),(row+2,col+1),(row+1,col+2),(row,col+3)}"
+            else:
+                temp = temp_down
+                address = f"Down Diag {(row, col),(row+1,col+1),(row+2,col+2),(row+3,col+3)}"
+            l.append([temp,temp_up,temp_down])
             if temp > prd:
-                prd == temp
-            print(f"current max product {prd}, at row {row} and col {col}")
+                prd = temp
+                d = {"product": prd,
+                  "address": address}
 
-
+    return d
 
 
