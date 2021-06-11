7c5f072bdbc2d09746fb7858d5770bf47e09cf62
Abhishek Bansal <abhishek.oneplus.0105@gmail.com>
2021-06-11 23:31:05 +0530

    some chnages to test configs

--- BSVanillaOptions.py
+++ BSVanillaOptions.py
@@ -3,17 +3,6 @@ import pandas as pd
 from scipy.stats import norm
 
 
-# def forward_PriceBS(spot, rate, maturity, dividend=None, time=None):
-#
-#     if not dividend is None and not time is None:
-#         if not len(dividend) == len(time):
-#             return print("dividends and payment time not match")
-#         else:
-#             D = sum([dividend[x]*np.exp(rate*(maturity-time[x])) for x in range(0,len(dividend))])
-#     else:
-#         D = 0
-#     # Theorem 2.1
-#     return spot*np.exp(rate*maturity) - D
 
 def forward_valueBS(spot, rate, maturity, strike):
 
