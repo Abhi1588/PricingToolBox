36134d709c962a04052a005904f09562d7f3c9d5
Abhishek Bansal <abhishek.oneplus.0105@gmail.com>
2021-06-14 22:18:48 +0530

    some changes in the B3 funtions file

--- B3_VanillaOptionsinBlackScholesWorld.py
+++ B3_VanillaOptionsinBlackScholesWorld.py
@@ -38,73 +38,73 @@ def zerocouponbond(rate,maturity):
 
 
 
+def main():
+    spot = 100
+    strike = 100
+    maturity = 1
+    rate = 0.02
+    dividend = 0
+    vol = .05
 
-spot = 100
-strike = 100
-maturity = 1
-rate = 0.02
-dividend = 0
-vol = .05
 
+    # Put Call Parity
+    put = europeanPutOptionPrice(spot,strike,maturity,rate,dividend,vol)
+    call = europeanCallOptionPrice(spot,strike,maturity,rate,dividend,vol)
+    fwd = forwardPrice(spot,strike,maturity,rate,dividend)
+    print("Put Call Parity \nCall :{} - Put :{} = {} \nForward: {}".format(call,put,call-put,fwd))
+    print("+"*20)
+    #Price of call is monotonically decreasing in strike
+    lStrike = []
+    lcallPrice = []
+    for i in range(strike-90, strike+110, 10):
+        lStrike.append(i)
+        lcallPrice.append(europeanCallOptionPrice(spot,i,maturity,rate,dividend,vol))
 
-# Put Call Parity
-put = europeanPutOptionPrice(spot,strike,maturity,rate,dividend,vol)
-call = europeanCallOptionPrice(spot,strike,maturity,rate,dividend,vol)
-fwd = forwardPrice(spot,strike,maturity,rate,dividend)
-print("Put Call Parity \nCall :{} - Put :{} = {} \nForward: {}".format(call,put,call-put,fwd))
-print("+"*20)
-#Price of call is monotonically decreasing in strike
-lStrike = []
-lcallPrice = []
-for i in range(strike-90, strike+110, 10):
-    lStrike.append(i)
-    lcallPrice.append(europeanCallOptionPrice(spot,i,maturity,rate,dividend,vol))
+    fig, ax = plt.subplots()
+    ax.plot(lStrike, lcallPrice, label = "call price")
+    ax.set_xlabel('strikes')  # Add an x-label to the axes.
+    ax.set_ylabel('option price')  # Add a y-label to the axes.
+    ax.set_title("Call Option Price vs Strike")  # Add a title to the axes.
+    ax.legend()
+    plt.show()
+    print("+"*20)
 
-fig, ax = plt.subplots()
-ax.plot(lStrike, lcallPrice, label = "call price")
-ax.set_xlabel('strikes')  # Add an x-label to the axes.
-ax.set_ylabel('option price')  # Add a y-label to the axes.
-ax.set_title("Call Option Price vs Strike")  # Add a title to the axes.
-ax.legend()
-plt.show()
-print("+"*20)
+    #Price of call is between S and S - k e^(-rt)
 
-#Price of call is between S and S - k e^(-rt)
+    f = spot - strike*math.exp(-rate*maturity)
 
-f = spot - strike*math.exp(-rate*maturity)
+    if f < call and call < spot:
+        print("True")
 
-if f < call and call < spot:
-    print("True")
+    print("+"*20)
 
-print("+"*20)
+    #Price of call is monotonically increasign in vol
+    lvol = []
+    lcallPrice = []
+    for i in np.arange(vol*.5, vol*1.5, 0.005):
+        lvol.append(i)
+        lcallPrice.append(europeanCallOptionPrice(spot,strike,maturity,rate,dividend,i))
 
-#Price of call is monotonically increasign in vol
-lvol = []
-lcallPrice = []
-for i in np.arange(vol*.5, vol*1.5, 0.005):
-    lvol.append(i)
-    lcallPrice.append(europeanCallOptionPrice(spot,strike,maturity,rate,dividend,i))
+    fig, ax = plt.subplots()
+    ax.plot(lvol, lcallPrice, label = "call price")
+    ax.set_xlabel('vol')  # Add an x-label to the axes.
+    ax.set_ylabel('option price')  # Add a y-label to the axes.
+    ax.set_title("Call Option Price vs vol")  # Add a title to the axes.
+    ax.legend()
+    plt.show()
+    print("+"*20)
 
-fig, ax = plt.subplots()
-ax.plot(lvol, lcallPrice, label = "call price")
-ax.set_xlabel('vol')  # Add an x-label to the axes.
-ax.set_ylabel('option price')  # Add a y-label to the axes.
-ax.set_title("Call Option Price vs vol")  # Add a title to the axes.
-ax.legend()
-plt.show()
-print("+"*20)
+    Dcall = digitalCall(spot,strike,maturity,rate,dividend,vol)
+    Dput = digitalPut(spot,strike,maturity,rate,dividend,vol)
+    zcb = zerocouponbond(rate,maturity)
 
-Dcall = digitalCall(spot,strike,maturity,rate,dividend,vol)
-Dput = digitalPut(spot,strike,maturity,rate,dividend,vol)
-zcb = zerocouponbond(rate,maturity)
+    call_short = europeanCallOptionPrice(spot,strike+1,maturity,rate,dividend,vol)
 
-call_short = europeanCallOptionPrice(spot,strike+1,maturity,rate,dividend,vol)
+    spread = call - call_short
 
-spread = call - call_short
+    print(Dcall+Dput, "ZCB : {}".format(zcb))
 
-print(Dcall+Dput, "ZCB : {}".format(zcb))
-
-print(Dcall,"Spread: {}".format(spread))
+    print(Dcall,"Spread: {}".format(spread))
 
 
 
