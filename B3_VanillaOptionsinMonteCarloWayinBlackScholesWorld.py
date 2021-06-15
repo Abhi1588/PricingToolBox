5cc78cbaf4efbf9ef40ec88cf3efef10fec3db29
Abhishek Bansal <abhishekbansal@MacBookAir.local>
2021-06-15 11:39:46 +0530

    Added all payoffs

--- B3_VanillaOptionsinMonteCarloWayinBlackScholesWorld.py
+++ B3_VanillaOptionsinMonteCarloWayinBlackScholesWorld.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 import B3_VanillaOptionsinBlackScholesWorld as B3
 
 
-
 class MonteCarloOptionPricer:
     def __init__(self):
         self.assetPrice = None
@@ -20,24 +19,46 @@ class MonteCarloOptionPricer:
         S_T[:, 0] = spot
         if dividend is None:
             dividend = 0
-        S_T[:,1] = (S_T[:,0]*np.exp((rate-dividend)*maturity - 0.5*vol**2*maturity + vol*np.sqrt(maturity)*rand))
+        S_T[:, 1] = (S_T[:, 0] * np.exp(
+            (rate - dividend) * maturity - 0.5 * vol ** 2 * maturity + vol * np.sqrt(maturity) * rand))
         self.assetPrice = S_T
         self.discountFactor = self.zerocouponbond(rate, maturity)
 
     def zerocouponbond(self, rate, maturity):
         return math.exp(-rate * maturity)
 
-    def option_pricer_GBM(self, payoffObj, strike, paths = "All"):
+    def option_pricer_GBM(self, payoffObj, strike, paths="All"):
         if paths == "All":
             payoff = payoffObj(self.assetPrice[:, 1], strike)
         else:
             payoff = payoffObj(self.assetPrice[:paths, 1], strike)
-        self.optionPrice = (payoff*self.discountFactor).mean()
+        self.optionPrice = (payoff * self.discountFactor).mean()
 
 
 def call_payoff(prices, strike):
-    return numpy.maximum((prices - strike),0)
+    return numpy.maximum((prices - strike), 0)
 
+
+def put_payoff(prices, strike):
+    return numpy.maximum((strike - prices), 0)
+
+
+def forward_payoff(prices, strike):
+    return prices - strike
+
+
+def digitalCall_Payoff(prices, strike):
+    if prices - strike > 0:
+        return 1
+    else:
+        return 0
+
+
+def digitalPut_Payoff(prices, strike):
+    if strike - prices > 0:
+        return 1
+    else:
+        return 0
 
 
 spot = 100
@@ -49,12 +70,10 @@ vol = .10
 noOfSim = 10000
 
 callMC = MonteCarloOptionPricer()
-callMC.simulateAssetPrices_GBM(spot,rate,maturity,vol,noOfSim,dividend)
-callMC.option_pricer_GBM(call_payoff,strike)
+callMC.simulateAssetPrices_GBM(spot, rate, maturity, vol, noOfSim, dividend)
+callMC.option_pricer_GBM(call_payoff, strike)
 print(callMC.optionPrice)
 
-
-
-call = B3.europeanCallOptionPrice(spot,strike,maturity,rate,dividend,vol)
+call = B3.europeanCallOptionPrice(spot, strike, maturity, rate, dividend, vol)
 
 print(call)
